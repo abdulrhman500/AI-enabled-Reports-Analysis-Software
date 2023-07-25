edb5c141d30b45d9a29ccd2f9754158c9532f1f3
Hoda-Desouki <hoda2750@hotmail.com>
2023-07-25 17:44:46 +0300

    extract tasks section using RegExr

--- Data Extraction/extract.py
+++ Data Extraction/extract.py
@@ -3,21 +3,21 @@ import re
 
 def getStartPosition(extracted_text):
     start_position = -1
-    tasks_pattern = r'\bInternship\s+Performed\s+Tasks\b'
+    tasks_pattern = 'Internship\s[a-z]*[\s]*Tasks|Internship\sactivities'
     tasks_match = re.search(tasks_pattern, extracted_text, re.IGNORECASE | re.DOTALL)
 
     if tasks_match:
         start_position = tasks_match.end()
-    
     return start_position
 
 def getEndPosition(extracted_text):
-    header_text ="Internship Evaluation"
+    # header_text ="Internship Evaluation"
     end_position = -1
-    end_pattern = r'\bInternship\s+Evaluation\b'
+    end_pattern = 'Evaluation\sof\s[a-z]*[\s]*Internship|Internship\sEvaluation'
     end_match = re.search(end_pattern, extracted_text, re.IGNORECASE | re.DOTALL)
     if end_match:
-        end_position = end_match.end() - len(header_text)
+        end_position = end_match.start()
+        # - len(header_text)
 
     return end_position
 
@@ -34,6 +34,6 @@ def getTextFromFile(path):
 
     return text_after_tasks
 
-pdf_file_path = "Ahmed Abdelazeem Omar 40-13742 - Ahmad Kholy.pdf"
+pdf_file_path = '(A) Ahmed Abdelazeem Omar 40-13742.pdf'
 extracted_text = getTextFromFile(pdf_file_path)
 print(extracted_text)
