3b3c14fe7464471e3eb07a8369ca0240b9c9c83e
Abdulrahman Fahmy <abdulrhmanabotor@gmail.com>
2023-07-31 17:42:18 +0300

    extraction semi final

--- src/Data Extraction/extract.py
+++ src/Data Extraction/extract.py
@@ -1,20 +1,43 @@
 import string
 import re
 import extract_unscanned_pdf
+import extract_scanned_pdf
+
 import os
 def extract_unscanned_text(file_path)-> string :
    return extract_unscanned_pdf.get_text(file_path) 
 
 def extract_scanned_text(file_path)-> string:
-    pass
-def is_sccanned(file_path)-> bool:
-    pass
+    extract_scanned_pdf.get_text(file_path)
+
+def is_scanned_pdf(pdf_path, start_page=4, end_page=None, max_words_threshold=50):
+    try:
+        with fitz.open(pdf_path) as doc:
+            if end_page is None:
+                end_page = doc.page_count
+
+            for page_number in range(start_page, end_page):
+                page = doc.load_page(page_number)
+                text = page.get_text()
+
+                # Count the number of words in the page
+                num_words = len(text.split())
+
+                # If the number of words is below the threshold, consider it as a scanned page
+                if num_words <= max_words_threshold:
+                    return True
+
+            return False
+
+    except Exception as e:
+        print(f"Error checking if PDF is scanned: {e}")
+        return False
 
 
 def extract_single_text(file_path)-> string:
-    # if is_sccanned(file_path):
-        # return extract_scanned_text(file_path)
-    # else:
+    if is_scanned_pdf(file_path):
+        return extract_scanned_text(file_path)
+    else:
         return extract_unscanned_text(file_path)
        
 def get_pdfs_paths_in_folder(folder_path):
@@ -32,15 +55,21 @@ def save_text_to_file(folder_name, file_name, text):
     with open(file_path, 'w',encoding='utf-8') as file:
         file.write(text)
 
-def extract_samples(folder_path) -> None:
-    files_paths = get_pdfs_paths_in_folder(folder_path)
+def extract_samples(source_folder_path,destination_folder) -> None:
+    files_paths = get_pdfs_paths_in_folder(source_folder_path)
     for file_path in files_paths:
         print(file_path,"*************")
         text =  extract_single_text(file_path)
         file_name = file_path[:-3]
+        arr = file_name.split('\\')
+        file_name = arr[len(arr)-1]
         file_name += "txt"
-        # print(text)
-        save_text_to_file(folder_path,file_name,text)
-    
-extract_samples("E:\\NLP\\Internship\\Dirty Reports\\Dirty Files\\correct template")
+        print("666666666666",file_name)
+        
+        save_text_to_file(destination_folder,file_name,text)
+
+        
+source = "E:\\NLP\\Internship\\AI-enabled-Reports-Analysis-Software\\Sample_PDFs"
+destination = "E:\\NLP\\Internship\\AI-enabled-Reports-Analysis-Software\\src\\Data Extraction\\Dirty Samples"
+extract_samples(source,destination)
 
--- src/Data Extraction/extract_unscanned_pdf.py
+++ src/Data Extraction/extract_unscanned_pdf.py
@@ -1,31 +1,137 @@
 import re
 import string
-from PyPDF2 import PdfReader
-import get_start_end
+import fitz
+import PyPDF2
 
-def extract_unscanned_text(file_path)-> string:
-    reader = PdfReader(file_path)
-    extracted_text = ""
-    for page in reader.pages:
-        extracted_text += page.extract_text()
+
+def is_bold(font_flags):
+    return bool(font_flags & (1 << 4))
+
+
+def find_positions(regex_pattern, text):
+    positions = []
+    for match in re.finditer(regex_pattern, text, re.IGNORECASE):
+        print("MATCH####", match.group())
+        positions.append(match.start())
+    return positions
+
+
+def check(terminal_list, file_path, regex):
+    bolds, count_list, count = extract_bold_text_with_regex(file_path, regex)
+    print(bolds, "---------------")
+    if len(count_list) == 1:
+        print("Terminal list", terminal_list)
+        print("count list", count_list)
+        print("RET", [terminal_list[count_list[0]-1]])
+        if(count_list[0]-1<len(terminal_list) and count_list[0]-1>=0 ):
+            return [terminal_list[count_list[0]-1]]
+    else:
+        print("Terminal list", terminal_list)
+        print("count list", count_list)
+        if (len(count_list) < 1):
+            return []
+        ret = []
+        for idx in count_list:
+            if(idx - 1 < len(terminal_list)):
+                ret.append(terminal_list[idx-1])
+        # ret = [terminal_list[idx - 1] for idx in count_list]
+        print("RET2", ret)
+        return ret
+
+
+def extract_bold_text_with_regex(pdf_file_path, regex_pattern):
+    count = 0
+    count_list = []
+    bold_text_with_regex = []
+    doc = fitz.open(pdf_file_path)
+
+    for page_number in range(doc.page_count):
+        page = doc.load_page(page_number)
+        blocks = page.get_text("dict", flags=11)["blocks"]
+        for block in blocks:
+            for line in block["lines"]:
+                for span in line["spans"]:
+                    if re.findall(regex_pattern, span["text"], re.IGNORECASE):
+                        match = re.findall(regex_pattern, span["text"], re.IGNORECASE)
+                        # print("MATCH LEN",len(match))
+                        count += len(match)
+                        print("Count",count)
+                        print(span["text"])
+                        if is_bold(span["flags"]):
+                            count_list.append(count)
+                            print("count list", count_list)
+                            bold_text_with_regex.append(span["text"])
+
+
+    doc.close()
+    return bold_text_with_regex, count_list, count
+
+
+def extract_text_from_PDF(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        full_text = ""
+        for page in pdf_reader.pages:
+            full_text += page.extract_text()
+    return full_text
+
+
+
+def get_start_end(pdf_file_path):
+    regex_pattern_start1 = "task"
+    regex_pattern_start2 = "activities"
+    regex_pattern_end = "evaluation"
+
+    text = extract_text_from_PDF(pdf_file_path)
+    pos_start1 = find_positions(regex_pattern_start1, text)
+    pos_start2 = find_positions(regex_pattern_start2, text)
+    pos_end = find_positions(regex_pattern_end, text)
+    print("pos_start1", pos_start1)
+    print("pos_start2", pos_start2)
+    print("pos_end ", pos_end)
+    start1 = check(pos_start1, pdf_file_path, regex_pattern_start1)
+    start2 = check(pos_start2, pdf_file_path, regex_pattern_start2)
+    end = check(pos_end, pdf_file_path, regex_pattern_end)
+    print("start1", start1)
+    print("start2 ", start2)
+    print("end ", end)
+
+    if (len(start2) > 0):
+        s = start2[len(start2)-1]
+    elif (len(start1) > 0):
+        s = start1[len(start1)-1]
+    else:
+        s = -1
+    if (len(end) > 0):
+        e = end[len(end)-1]
+    else:
+        e = -1
+    print(s)
+    print(e)
+    return s,e
+
+
+def extract_text_from_headline(pdf_path, start, end):
+    with open(pdf_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        full_text = ""
+        for page in pdf_reader.pages:
+            full_text += page.extract_text()
+    extracted_text = full_text[start:end]
     return extracted_text
-count = 0
-def get_text(file_path)->string:
-    extracted_text = extract_unscanned_text(file_path)
-    choosen_start, choosen_end=0,0
-    tasks_text=''
-    # try:
-    choosen_start, choosen_end = get_start_end.choose_start_end(file_path)
-    
-    tasks_text = extracted_text[choosen_start:choosen_end]
 
-    # except Exception as e:
-        # print("\033[31m"+"Error : "+file_path+"\033[31m",e)
+def get_text(file_path)->string:
+    start, end=0,0
+    tasks_text=''
+    try:
+        start, end = get_start_end(file_path)
+        tasks_text = extract_text_from_headline(file_path,start,end)
+    except Exception as e:
+        print("[ERROR] ",e)
     
     return tasks_text
 
 if __name__ == "__main__":
-    pass
-    print("\nTASKS:\n\n"+get_text("E:\\NLP\\Internship\\Dirty Reports\\Dirty Files\\correct template\\(A) MariamAdelAbbasAbdelfattah_43-2045 - Mariam Enany.pdf"))
-
+    pdf_file_path = "C:\\Users\\hoda2\\Documents\\NLP Internship\\Dirty Files\\correct template\\TASKS PERFORMED (A) Maryam Khaled, 43-7239 - Maryam Amin.pdf"
+    print("\nTASKS:\n\n"+get_text(pdf_file_path))
 
