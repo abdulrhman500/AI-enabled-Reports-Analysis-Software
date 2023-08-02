3461a6f6859c5103c4a7a93e87d5a460b75fd3d1
Abdulrahman Fahmy <abdulrhmanabotor@gmail.com>
2023-08-02 09:38:17 +0300

    Delete new_bold.py

--- src/Data Extraction/new_bold.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber
-file_to_parse = "E:\\NLP\\Internship\\Dirty Reports\\Dirty Files\\correct template\\(A) 1-merged - Rawan Ahmad.pdf"
-with pdfplumber.open(file_to_parse) as pdf: 
-    text = pdf.pages[3]
-    clean_text = text.filter(lambda obj: not (obj["object_type"] == "char" and "Bold" in obj["fontname"]))
-    print(clean_text.extract_text())
-
