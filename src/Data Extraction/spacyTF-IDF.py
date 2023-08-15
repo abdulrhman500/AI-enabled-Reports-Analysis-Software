165291e1176f97248e447d92f301f6dc663c1e48
Hoda-Desouki <hoda2750@hotmail.com>
2023-08-15 19:47:58 +0300

    Perform Feature Engineering on Samples

---
+++ src/Data Extraction/spacyTF-IDF.py
@@ -0,0 +1,32 @@
+import spacy
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+import os
+import glob
+import pandas as pd
+def preprocess(text):
+    text = re.sub('\n', '', text)
+    nlp = spacy.load("en_core_web_lg")
+    doc = nlp(text)
+    cleanedtext = [item.lemma_ for item in doc if not item.is_stop and not item.is_punct and not item.is_space]     
+    return " ".join(cleanedtext)
+
+folder_path = 'C:\\Users\\hoda2\\Documents\\NLP Internship\\AI-enabled-Reports-Analysis-Software\\src\\Data Extraction\\Clean Samples'
+file_extension = '*.txt'
+
+corpus = []
+for file_path in glob.glob(os.path.join(folder_path, file_extension)):
+    with open(file_path, encoding="utf8") as file:
+        text = file.read()
+        corpus.append(preprocess(text))
+
+v = TfidfVectorizer()
+v.fit(corpus)
+transformed_output = v.fit_transform(corpus)
+feature_names = v.get_feature_names_out()
+dense = transformed_output.todense()
+lst1 = dense.tolist()
+
+df = pd.DataFrame(lst1, columns=feature_names)
+
+tf_idf_counter = df.T.sum(axis=1)
