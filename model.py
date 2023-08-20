69111d0b04b52ccf1051c06d195a8dc62212c229
Mariam <mariamsherif88@icloud.com>
2023-08-21 02:54:19 +0300

    undersampling accept and oversampling reject,pending

--- model.py
+++ model.py
@@ -2,30 +2,32 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
 import ast
 
 # Load the CSV file
 csv_file_path = "C:\\Users\\DELL\\Desktop\\USE\\AI-enabled-Reports-Analysis-Software\\Dataset.csv"
 df = pd.read_csv(csv_file_path)
 
-# Convert string representation of lists to actual lists
 df['Embedding'] = df['Embedding'].apply(ast.literal_eval)
 
-# Separate features (embeddings) and labels
 X = df['Embedding'].tolist()
 y = df['label'].tolist()
 
-# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Initialize and train the multinomial logistic regression model
-model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
-model.fit(X_train, y_train)
+oversampler = SMOTE(sampling_strategy={'Reject': 1000, 'Pending': 1000})
+X_train_resampled, y_train_resampled = oversampler.fit_resample(X_train, y_train)
 
-# Make predictions on the test set
+undersampler = RandomUnderSampler(sampling_strategy={'Approved': 100})
+X_train_final, y_train_final = undersampler.fit_resample(X_train_resampled, y_train_resampled)
+
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+model.fit(X_train_final, y_train_final)
+
 y_pred = model.predict(X_test)
 
-# Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
 classification_rep = classification_report(y_test, y_pred)
 
