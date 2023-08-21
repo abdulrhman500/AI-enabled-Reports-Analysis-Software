e702bce9ac0e647d95f61a8c38f76b38fcb693fa
Maryam Sherif <mariamsherif2.ms@gmail.com>
2023-08-22 00:59:05 +0300

    extractUnscanned

--- src/Data Extraction/extract_scanned_pdf.py
+++ src/Data Extraction/extract_scanned_pdf.py
@@ -6,7 +6,7 @@ from PIL import Image
 from pdf2image import convert_from_path
 
 def get_text(pdf_path):
-    images = convert_from_path(pdf_path, poppler_path="C:/Users/Ahmed Hatem/poppler-23.08.0/Library/bin")
+    images = convert_from_path(pdf_path)
     # Specify the page number to start from
     start_page = 4  # Change this to the desired start page number
 
@@ -15,8 +15,8 @@ def get_text(pdf_path):
     for page_num, image in enumerate(images, start=1):
         if page_num < start_page:
             continue  # Skip pages before the start page
-
-        text = pytesseract.image_to_string(image, lang='eng')
+        #pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+        text = pytesseract.image_to_string(image, lang='eng', )
         extracted_text += text + "\n"
 
     # Specify a regular expression pattern to match bold titles
@@ -39,13 +39,16 @@ def get_text(pdf_path):
             current_section["content"] += line.strip() + " "
 
     # Search for a section title that contains "Internship Performed Tasks:" within its title
-    target_section_title = "Internship Performed Tasks:"
+    target_section_title = "Internship Performed Tasks"
+    target_section_title2 = "Internship Activities"
     target_section_content = ""
     max_content_length = 0
 
     for section in sections:
+        
         # check if the target section title is found within the section title
-        if target_section_title.lower() in section["title"].lower():
+        if target_section_title.lower() in section["title"].lower() or target_section_title2.lower() in section["title"].lower():
+
             # calculate content length (excluding the title)
             content_length = len(section["content"]) - len(target_section_title)
             
@@ -54,12 +57,12 @@ def get_text(pdf_path):
                 max_content_length = content_length
                 target_section_content = section["content"]
                 target_section_title = section["title"]
+                
             #check if the "Internship Performed Tasks:" title contains more than 3 words and if it does: 
             #add the extra words at the start of the content
             #and remove them from the title and remove "Internship Performed Tasks:" from the start of the content
             if len(target_section_title.split()) > 3:
                 target_section_content = target_section_title + " " + target_section_content
-                target_section_title = "Internship Performed Tasks:"
                 target_section_content = target_section_content.replace("Internship Performed Tasks:", "")
             
             #check if the content contains "Internship Evaluation" and if it does:
@@ -71,27 +74,22 @@ def get_text(pdf_path):
             #remove it and everything before it from the content
             if "Internship Performed Tasks:" in target_section_content:
                 target_section_content = target_section_content.split("Internship Performed Tasks:")[1]
-    
+
     if not target_section_content:
-        target_section_content = None
+            #get the content with the highest length that contains "Internship Performed Tasks:" or "Internship Activities" in its content
+            maxLength=0
+            for section in sections:
+                if target_section_title.lower() in section["content"].lower() or target_section_title2.lower() in section["content"].lower():
+                    if len(section["content"]) > maxLength:
+                        maxLength=len(section["content"])
+                        target_section_content=section["content"]
+
+            undesired="your future career"
+            if undesired.lower() in target_section_content.lower():
+                target_section_content=None
+
+            if not target_section_content:
+                target_section_content=None
+
 
     return target_section_content
-
-# Loop through all the pdf files in the folder 
-# and extract the text from the pdf files
-# and save the text in a text file
-
-# import os
-# path = "C:/Users/maria/OneDrive/Desktop/Internship Project/Dirty Reports/Dirty Files/OCR/working"
-# for filename in os.listdir(path):
-#     if filename.endswith(".pdf"):
-#         pdf_path = os.path.join(path, filename)
-#         text = get_text(pdf_path)
-#         if text:
-#             text_file = open(os.path.join(path, filename.replace(".pdf", ".txt")), "w")
-#             text_file.write(text)
-#             text_file.close()
-#         else:
-#             print("No text found in: " + filename)
-#     else:
-#         continue
