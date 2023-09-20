4e1e92e7e832decfe2d341aceafa79719336b996
Hoda-Desouki <hoda2750@hotmail.com>
2023-09-20 10:40:03 +0300

    modify

--- src/Data Extraction/constants.py
+++ src/Data Extraction/constants.py
@@ -2,9 +2,6 @@ SOURCE_FOLDER_PATH= "../../Sample_PDFs"
 EXTRACTED_SAMPLEs_FOLDER ="src\\Data Extraction\\Dirty Samples"
 CLEAN_SAMPLES_FOLDER="src\\Data Extraction\\Clean Samples"
 
-## local
-TEST_PDF_FILE_PATH="Sample_PDFs\\Abdel-Haliem Ahmed Shokeir 31-5774\\Abdel-Haliem Ahmed Shokeir 31-5774 - Mr Leeb.pdf"
-CORRECT_SAMPLES_PATH="E:\\NLP\\Internship\\Dirty Reports\\Dirty Files\\correct template"
 
 ## constant Strings
 TASKS_SECTION_HEADLINE = "Internship Performed Tasks"
